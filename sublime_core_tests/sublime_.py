10da273680b4eacca10303230273be3a805badd3
Todd Wolfson <todd@twolfson.com>
2016-08-22 15:16:04 +0200

    Added more schema tests

--- sublime_core_tests/sublime_.py
+++ sublime_core_tests/sublime_.py
@@ -24,6 +24,42 @@ class TestSublimeSetAsyncTimeout(unittest.TestCase):
         # TODO: Test set_async_timeout call itself
         self.assertEquals(hasattr(sublime, 'set_async_timeout'), True)
 
+class TestSublimeErrorMessage(unittest.TestCase):
+    def test_sublime_error_message(self):
+        """
+        A sublime package
+            has a method for `error_message`
+        """
+        # DEV: We cannot fully test `error_message` as it generates a UI dialog
+        self.assertEquals(hasattr(sublime, 'error_message'), True)
+
+class TestSublimeMessageDialog(unittest.TestCase):
+    def test_sublime_message_dialog(self):
+        """
+        A sublime package
+            has a method for `message_dialog`
+        """
+        # DEV: We cannot fully test `message_dialog` as it generates a UI dialog
+        self.assertEquals(hasattr(sublime, 'message_dialog'), True)
+
+class TestSublimeOkCancelDialog(unittest.TestCase):
+    def test_sublime_ok_cancel_dialog(self):
+        """
+        A sublime package
+            has a method for `ok_cancel_dialog`
+        """
+        # DEV: We cannot fully test `ok_cancel_dialog` as it generates a UI dialog
+        self.assertEquals(hasattr(sublime, 'ok_cancel_dialog'), True)
+
+class TestSublimeYesNoCancelDialog(unittest.TestCase):
+    def test_sublime_yes_no_cancel_dialog(self):
+        """
+        A sublime package
+            has a method for `yes_no_cancel_dialog`
+        """
+        # DEV: We cannot fully test `yes_no_cancel_dialog` as it generates a UI dialog
+        self.assertEquals(hasattr(sublime, 'yes_no_cancel_dialog'), True)
+
 class TestSublimeVersion(unittest.TestCase):
     def test_sublime_version(self):
         """
