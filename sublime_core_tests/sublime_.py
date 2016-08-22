fbab1322f8a370b09ce6c8c300be7976f96262dd
Todd Wolfson <todd@twolfson.com>
2016-08-22 15:09:24 +0200

    Added 2 new test cases

--- sublime_core_tests/sublime_.py
+++ sublime_core_tests/sublime_.py
@@ -1,11 +1,30 @@
 # Load in our dependencies
 # DEV: We can't name this file `sublime.py` otherwise we get issues with UnitTesting
 import sublime
-from unittest import TestCase
+import unittest
 
 # Define our tests
-# TODO: Collect untested methods
-class TestSublimeVersion(TestCase):
+class TestSublimeSetTimeout(unittest.TestCase):
+    def test_sublime_set_timeout(self):
+        """
+        A sublime package
+            has a method for `set_timeout`
+        """
+        # TODO: Test set_timeout call itself
+        self.assertEquals(hasattr(sublime, 'set_timeout'), True)
+
+class TestSublimeSetAsyncTimeout(unittest.TestCase):
+    # TODO: Figure out why we cannot find `set_async_timeout`
+    @unittest.skip('`set_async_timeout` doesn\'t seem to exist in Sublime Text 3114')
+    def test_sublime_set_async_timeout(self):
+        """
+        A sublime package
+            has a method for `set_async_timeout`
+        """
+        # TODO: Test set_async_timeout call itself
+        self.assertEquals(hasattr(sublime, 'set_async_timeout'), True)
+
+class TestSublimeVersion(unittest.TestCase):
     def test_sublime_version(self):
         """
         A sublime package
